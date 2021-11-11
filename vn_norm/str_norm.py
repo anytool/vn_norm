4cb839f84f992a93e112b706963ea93da9e4e0da
Tri Toan <nguyentritoanst@gmail.com>
2021-11-11 22:01:12 +0700

    test

--- vn_norm/str_norm.py
+++ vn_norm/str_norm.py
@@ -1,67 +1,92 @@
 import re
 import numpy as np
 
-_unit_map = [
-    # unit
-    (re.compile("([\W0-9])(%s)(\\W)" %
-     x[0], re.IGNORECASE), r"\1 %s \3" % x[1])
-    for x in [
-        ("mm3/s", "mi li mét khối trên giây"),
-        ("cm3/s", "cen ti mét khối mỗi giây"),
-        ("l/min", "lít mỗi phút"),
-        ("km3/s", "kí lô mét khối trên giây"),
-        ("in3/s", "inch khối trên giây"),
-        ("in3/h", "inch khối một giờ"),
-        ("km/h", "kilômét trên giờ"),
-        ("ml/s", "ml mỗi giây"),
-        ("m3/s", "mét khối mỗi giây"),
-        ("m3/h", "mét khối một giờ"),
-        ("mm2", "mi li mét vuông"),
-        ("cm2", "cen ti mét vuông"),
-        ("km2", "kí lô mét vuông"),
-        ("mm3", "mi li mét khối"),
-        ("cm3", "xăng-ti-mét khối"),
-        ("km3", "kí lô mét khối"),
-        ("mwh", "mi li wat giờ"),
-        ("kwh", "kí lô wat giờ"),
-        ("mwh", "mega wat giờ"),
-        ("gwh", "gi ga wat giờ"),
-        ("khz", "kí lô hẹt"),
-        ("mm", "mi li mét"),
-        ("cm", "cen ti mét"),
-        ("km", "kí lô mét"),
-        ("m2", "mét vuông"),
-        ("ha", "héc ta"),
-        ("kg", "kí lô gram"),
-        ("oz", "ounce"),
-        ("lb", "pao"),
-        ("ml", "mi li lít"),
-        ("m3", "mét khối"),
-        ("kb", "kí lô bai"),
-        ("mb", "mê ga bai"),
-        ("gb", "gi ga bai"),
-        ("tb", "tê ra bai"),
-        ("mv", "mi li vôn"),
-        ("kv", "kí lô vôn"),
-        ("kw", "kí lô watt"),
-        ("gw", "gi ga wat"),
-        ("wh", "wat giờ"),
-    ]
-]
+# _unit_map = [
+#     # unit
+#     (re.compile("([\W0-9])(%s)(\\W)" %
+#      x[0], re.IGNORECASE), r"\1 %s \3" % x[1])
+#     for x in [
+#         ("mm3/s", "mi li mét khối trên giây"),
+#         ("cm3/s", "cen ti mét khối mỗi giây"),
+#         ("l/min", "lít mỗi phút"),
+#         ("km3/s", "kí lô mét khối trên giây"),
+#         ("in3/s", "inch khối trên giây"),
+#         ("in3/h", "inch khối một giờ"),
+#         ("km/h", "kilômét trên giờ"),
+#         ("ml/s", "ml mỗi giây"),
+#         ("m3/s", "mét khối mỗi giây"),
+#         ("m3/h", "mét khối một giờ"),
+#         ("mm2", "mi li mét vuông"),
+#         ("cm2", "cen ti mét vuông"),
+#         ("km2", "kí lô mét vuông"),
+#         ("mm3", "mi li mét khối"),
+#         ("cm3", "xăng-ti-mét khối"),
+#         ("km3", "kí lô mét khối"),
+#         ("mwh", "mi li wat giờ"),
+#         ("kwh", "kí lô wat giờ"),
+#         ("mwh", "mega wat giờ"),
+#         ("gwh", "gi ga wat giờ"),
+#         ("khz", "kí lô hẹt"),
+#         ("mm", "mi li mét"),
+#         ("cm", "cen ti mét"),
+#         ("km", "kí lô mét"),
+#         ("m2", "mét vuông"),
+#         ("ha", "héc ta"),
+#         ("kg", "kí lô gram"),
+#         ("oz", "ounce"),
+#         ("lb", "pao"),
+#         ("ml", "mi li lít"),
+#         ("m3", "mét khối"),
+#         ("kb", "kí lô bai"),
+#         ("mb", "mê ga bai"),
+#         ("gb", "gi ga bai"),
+#         ("tb", "tê ra bai"),
+#         ("mv", "mi li vôn"),
+#         ("kv", "kí lô vôn"),
+#         ("kw", "kí lô watt"),
+#         ("gw", "gi ga wat"),
+#         ("wh", "wat giờ"),
+#     ]
+# ]
 
 _abbreviations = [
     (re.compile("\\b%s\\." % x[0], re.IGNORECASE), x[1])
     for x in [
-        ("mrs", "bà"),
-        ("ms", "bà"),
-        ("mr", "ông"),
+        ("mrs", "misess"),
+        ("mr", "mister"),
+        ("dr", "doctor"),
+        ("st", "saint"),
+        ("co", "company"),
+        ("jr", "junior"),
+        ("maj", "major"),
+        ("gen", "general"),
+        ("drs", "doctors"),
+        ("rev", "reverend"),
+        ("lt", "lieutenant"),
+        ("hon", "honorable"),
+        ("sgt", "sergeant"),
+        ("capt", "captain"),
+        ("esq", "esquire"),
+        ("ltd", "limited"),
+        ("col", "colonel"),
+        ("ft", "fort"),
     ]
 ]
 
-_alias_map = np.concatenate((_abbreviations, _unit_map))
+_punc_map = [
+    # unit
+    (re.compile(x[0], re.IGNORECASE), x[1])
+    for x in [
+        ("...", "."),
+    ]
+]
+
+#  _unit_map
+_alias_map = np.concatenate((_abbreviations, _punc_map))
 
 
 def str_norm(text):
+    print('Hello')
     for regex, replacement in _alias_map:
         text = re.sub(regex, replacement, text)
     return text
